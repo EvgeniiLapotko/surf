f937afc98cdceb3d7f11bb7b072c70dfde106e55
Evgenii <lapotko.e@yandex.ru>
2021-01-26 17:27:58 +0300

    added section surf

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,21 @@ $(function () {
         slidesToScroll: 4,
         asNavFor: ".header__slider",
     });
+
+    $(".surf__slider").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:
+            '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt=""></img>',
+        nextArrow:
+            '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt=""></img>',
+        asNavFor: ".slider-map",
+    });
+
+    $(".slider-map").slick({
+        slidesToShow: 8,
+        asNavFor: ".surf__slider",
+        arrows: false,
+        focusOnSelect: true,
+    });
 });
